788f179b1bd3f71a471b498f2cb1beb8c758f4b4
lengyelluk <lengyel.luk@gmail.com>
2019-08-23 13:43:16 +0200

    sql to create tables and flyway added

---
+++ projects/P03-ReviewsAPI/src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,20 @@
+create table comment (
+    id int auto_increment,
+    comment_text varchar(500) not null,
+    created_time timestamp default CURRENT_TIMESTAMP,
+    review_id int not null,
+    PRIMARY KEY (id));
+
+create table product (
+    id int auto_increment,
+    name varchar(150) not null,
+    description varchar(255),
+    PRIMARY KEY (id));
+
+create table review (
+    id int auto_increment,
+    rating int not null,
+    review_text varchar(1500),
+    created_time timestamp default CURRENT_TIMESTAMP,
+    product_id int not null,
+    PRIMARY KEY (id));
